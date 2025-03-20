3be1e75f5102b98ce737b11f3ad2aa92c23d2211
sumittak119 <sumittak119@gmail.com>
2025-03-21 00:05:49 +0530

    first commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+      <footer className="bg-gray-900 text-white py-4 text-center">
+        <p>&copy; {new Date().getFullYear()} My Website. All rights reserved.</p>
+        <p> Designed by Sam</p>
+      </footer>
+    );
+  };
+  
+  export default Footer;
+  
